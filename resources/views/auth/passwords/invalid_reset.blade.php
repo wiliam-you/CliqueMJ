b8d63fcc3b7b7a9acc97c6b9255a8c60b574d932
william-you <wiliam_you@aliyun.com>
2019-02-09 18:49:31 +0800

    fix bugs

--- resources/views/auth/passwords/invalid_reset.blade.php
+++ resources/views/auth/passwords/invalid_reset.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-@include('includes.header')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+    @include('includes.header')
+    <section class="login_form">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Reset Password</div>
 
-                <div class="panel-body">
-                   <p style="color:RED">Your url is not valid, maybe is expired!</p>
+                        <div class="panel-body">
+                           <p style="color:RED">Your url is not valid, maybe is expired!</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
 @endsection
--- resources/views/auth/passwords/reset_success.blade.php
+++ resources/views/auth/passwords/reset_success.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-@include('includes.header')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+    @include('includes.header')
+    <section class="login_form">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Reset Password</div>
 
-                <div class="panel-body">
-                   <p>Success <a href="/login">login</a></p>
+                        <div class="panel-body">
+                           <p>Success <a href="/login">login</a></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
 @endsection
