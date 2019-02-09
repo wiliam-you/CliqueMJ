bba415e93bb18b250a39942ebb9c31a4c07573aa
william-you <wiliam_you@aliyun.com>
2019-02-09 18:42:30 +0800

    fix bugs

--- app/Http/Controllers/ForgetPasswordController.php
+++ app/Http/Controllers/ForgetPasswordController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\PiplModules\webservice\Models\ForgetPassword;
 use Carbon\Carbon;
+use App\User;
 
 class ForgetPasswordController extends Controller
 {
